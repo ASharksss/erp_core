7be216b4c70f5f548447a800dec7c22d697db3cb
Alsu Kurbanalieva <alsukurbanalieva035@gmail.com>
2024-10-30 16:59:38 +0300

    routing setup

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,62 @@
+const {Product, Category_product, Batch} = require("../models/models");
+
 class ProductController {
   async getProducts(req, res) {
     try {
-      return res.json('ляляля')
+      const products = await Product.findAll({
+        include: {
+          model: Category_product,
+          attributes: ['name']
+        },
+      })
+      return res.json(products)
     } catch (e) {
       return res.json({error: e.message})
     }
   }
+
+  async createProduct(req, res) {
+    try {
+      const {vendor_code, name, description, categoryProductId} = req.body
+      const product = await Product.create({vendor_code, name, description, categoryProductId})
+      return res.json(product)
+    } catch (e) {
+      return res.json({error: e.message})
+    }
+  }
+
+  async createProductCategory(req, res) {
+    try {
+      const {name} = req.body
+      const category = await Category_product.create({name})
+      return res.json(category)
+    } catch (e) {
+      return res.json({error: e.message})
+    }
+  }
+
+  async createProductComponent(req, res) {
+    try {
+      const {vendor_code, componentId, count} = req.body
+      const item = await Batch.create({vendor_code, count})
+      return res.json(item)
+    } catch (e) {
+      return res.json({error: e.message})
+    }
+  }
+
+  async createBatch(req, res) {
+    try {
+      const {vendor_code, count} = req.body
+      const batch = await Batch.create({vendor_code, count})
+
+      return res.json(batch)
+    } catch (e) {
+      return res.json({error: e.message})
+    }
+  }
+
+
 }
 
 module.exports = new ProductController()
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,6 +3,8 @@ const router = new Router()
 const productController = require('../controllers/productController')
 
 router.get('/getProducts', productController.getProducts)
+router.post('/createProduct', productController.createProduct)
+router.post('/createProductCategory', productController.createProductCategory)
 
 
 module.exports = router
